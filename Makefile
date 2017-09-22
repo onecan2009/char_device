a50f0af83f2168fbaee760824c20b6e98d8764b7
onecan2009 <oc1788@163.com>
2017-09-22 14:47:42 +0800

    char device init

---
+++ Makefile
@@ -0,0 +1,35 @@
+ifneq ($(KERNELRELEASE),) 
+#Kbuild语法的语句，指明模块源码中各个文件的依赖关系，以及生成的目标模块
+#$(warning  $(src))#/home/board/kernel1.3/KernelDriver
+#$(src) 是一个相对路径，它就是Makefile/Kbuild 文件所在的路径。同(obj) 就是编译目标保存的路径，默认就是源代码所在路
+
+MODULE_NAME := char_dev
+
+OBJDRIVER := char_net.o 
+
+
+
+$(MODULE_NAME)-objs := $(OBJDRIVER)
+obj-m := $(MODULE_NAME).o #生成模块 
+
+else
+DRIVERDIR := $(shell pwd)
+KERNELDIR = /home/rk3288-sdk/gpu-rk3288-kernel/
+
+
+all: clean
+
+ #$(MAKE) -C $(KERNELDIR) 指明跳转到内核源码目录下读取那里的Makefile 
+ #M=$(PWD) 意思是返回到当前目录继续读入、执行当前的Makefile。所以此文件执行两遍
+ #“M=”选项的作用是，当用户需要以某个内核为基础编译一个外部模块的话，需要在make modules 命令中加入“M=dir 
+ #modueles目标指向obj-m变量中设定的模块
+	$(MAKE) -C $(KERNELDIR) M=$(DRIVERDIR) modules
+
+	
+clean:
+	rm -rf *.o *.mod.c .*.cmd *.order *.symvers *.ko .tmp_versions
+	
+endif
+
+
+
---
+++ char_net.c
@@ -0,0 +1,131 @@
+#include <linux/module.h>  
+#include <linux/types.h>  
+#include <linux/fs.h>  
+#include <linux/errno.h>  
+#include <linux/mm.h>  
+#include <linux/sched.h>  
+#include <linux/init.h>  
+#include <linux/cdev.h>
+#include <asm/device.h>  //下面这三个头文件是由于动态创建需要加的
+#include <linux/device.h>
+#include <asm/io.h>  
+#include <asm/uaccess.h>  
+#include <linux/timer.h>  
+#include <asm/atomic.h>  
+#include <linux/slab.h>  
+#include <linux/device.h>  
+#include <linux/kernel.h>
+
+#define MAX_DEVICE_NUM 32768
+
+#define DEBUG_FLAG
+#ifdef DEBUG_FLAG
+    #define debug_print(fmt,arg...) printk(fmt,##arg)
+#else
+    #define debug_print(fmt,arg...) do{}while(0)
+#endif
+
+
+static int major;
+static struct cdev char_net_cdev;
+static struct class *cls;
+
+static int char_net_open(struct inode *inode, struct file *filp)
+{
+    debug_print("%s()\n",__func__);
+    
+    return 0;
+}
+
+static int char_net_close(struct inode *inode, struct file *filp)
+{
+    debug_print("%s()\n",__func__);
+    
+    return 0;
+}
+
+static ssize_t char_net_read(struct file *filp,char *buf,size_t count,loff_t *offp)
+{
+    debug_print("%s()\n",__func__);
+    
+    return 0;
+}
+
+static ssize_t char_net_write(struct file *filp,char *buf,size_t count,loff_t *offp)
+{
+    debug_print("%s()\n",__func__);
+    
+    return 0;
+}
+
+static long char_net_ioctl(struct file *file,unsigned int cmd, unsigned long arg)
+{
+    debug_print("%s()\n",__func__);
+    
+    return 0;
+}
+
+static struct file_operations char_net_fops = 
+{
+    .owner = THIS_MODULE,
+    .open = char_net_open,
+    .release = char_net_close,
+    .read = char_net_read,
+    .write = char_net_write,
+    .unlocked_ioctl = char_net_ioctl,
+
+};
+
+static int __init char_net_init(void)
+{
+    debug_print("%s()\n",__func__);
+    
+    dev_t devid;
+    int ret = -1;
+
+    if(major)
+    {
+        devid = MKDEV(major,0);
+        ret = register_chrdev_region(devid,1,"char_net");
+    }
+    else
+    {
+        ret = alloc_chrdev_region(&devid,0,MAX_DEVICE_NUM,"char_net");
+        if(ret < 0)
+        {
+            printk("error alloc ,ret is %d\n",ret);
+        }
+        major = MAJOR(devid);
+    }
+    
+    debug_print("devid is %d,major is %d\n",devid,major);
+    cdev_init(&char_net_cdev,&char_net_fops);
+    cdev_add(&char_net_cdev,devid,1);
+    
+    
+    cls = class_create(THIS_MODULE,"char_net");
+    device_create(cls,NULL,MKDEV(major,0),NULL,"char_net0");
+    
+    return 0;
+}
+
+static int __exit char_net_exit(void)
+{
+    debug_print("%s()\n",__func__);
+    
+    device_destroy(cls,MKDEV(major,0));
+    class_destroy(cls);
+    
+    cdev_del(&char_net_cdev);
+    unregister_chrdev_region(MKDEV(major,0),1);
+    
+    return 0;
+}
+
+
+module_init(char_net_init);  
+module_exit(char_net_exit);
+MODULE_LICENSE("GPL"); 
+
+
+
